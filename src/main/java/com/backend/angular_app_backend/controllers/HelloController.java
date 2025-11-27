3f2bafb0e8705b79f3888dd4ae94b5a166704806
weily10 <w3ily10@gmail.com>
2025-11-27 14:32:19 +0800

    update
    update controller

--- src/main/java/com/backend/angular_app_backend/controllers/HelloController.java
+++ src/main/java/com/backend/angular_app_backend/controllers/HelloController.java
@@ -2,8 +2,7 @@ package com.backend.angular_app_backend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
+ 
 
 @RestController
 public class HelloController {
