b8c1f44229b6f4d18f87febb5120097afa6247f5
weily10 <w3ily10@gmail.com>
2025-11-28 17:48:18 +0800

    add api

--- src/main/java/com/backend/angular_app_backend/Controller/ItemsController.java
+++ src/main/java/com/backend/angular_app_backend/Controller/ItemsController.java
@@ -5,12 +5,17 @@ import com.backend.Model.Item;
 import com.backend.angular_app_backend.Repository.ItemRepo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class ItemsController {
 
     private final ItemRepo itemRepository;
+    private List<Item> items = new ArrayList<>();
 
     public ItemsController(ItemRepo itemRepository) {
         this.itemRepository = itemRepository;
@@ -19,5 +24,11 @@ public class ItemsController {
     @GetMapping("/items")
     public List<Item> getItems() {
         return itemRepository.findAll();
+    }
+
+    @PostMapping("/items")
+    public Item addItem(@RequestBody Item item) {
+        items.add(item);
+        return item;
     }
 }
