9bd5a3a75fe22c57571fca56d31de4f744fee063
weily10 <w3ily10@gmail.com>
2025-12-01 15:05:01 +0800

    delete api done

--- src/main/java/com/backend/angular_app_backend/Controller/ItemsController.java
+++ src/main/java/com/backend/angular_app_backend/Controller/ItemsController.java
@@ -1,10 +1,13 @@
 package com.backend.angular_app_backend.Controller;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.Model.Item;
 import com.backend.angular_app_backend.Repository.ItemRepo;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -14,7 +17,8 @@ import java.util.List;
 public class ItemsController {
 
     private final ItemRepo itemRepository;
-     public ItemsController(ItemRepo itemRepository) {
+
+    public ItemsController(ItemRepo itemRepository) {
         this.itemRepository = itemRepository;
     }
 
@@ -23,9 +27,13 @@ public class ItemsController {
         return itemRepository.findAll();
     }
 
- 
     @PostMapping("/items")
     public Item addItem(@RequestBody Item item) {
         return itemRepository.save(item);
+    }
+
+    @DeleteMapping("items/{uuid}")
+    public void deleteItem(@PathVariable String uuid) {
+        itemRepository.deleteById(uuid);
     }
 }
